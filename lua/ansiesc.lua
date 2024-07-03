48baf31e3bd56ff05e92cbba8ab9e49bb1d30b8d
haoliang <haoliang0.1.2@gmail.com>
2024-07-04 00:31:10 +0800

    sync with upstream

--- lua/ansiesc.lua
+++ lua/ansiesc.lua
@@ -10,38 +10,24 @@ local ni = require("infra.ni")
 ---@return integer bufnr
 return function(name_or_nr, open_mode)
   local function namefn(bufnr) return string.format("ansi://%s/%s", name_or_nr, bufnr) end
-  local bufnr = Ephemeral({ namefn = namefn, modifiable = false, bufhidden = "hide" })
+  local bufnr = Ephemeral({ namefn = namefn, modifiable = false })
 
-  do
+  do --feed content to the terminal
     local tid = ni.open_term(bufnr, {})
-
-    --todo: it's a bug?
-    -- ni.create_autocmd("bufwipeout", { buffer = bufnr, once = true, callback = function() jelly.debug("bufwipeout") end })
-
-    ni.create_autocmd("bufhidden", {
-      buffer = bufnr,
-      once = true,
-      callback = function()
-        vim.schedule(function()
-          vim.fn.chanclose(tid)
-          ni.buf_delete(bufnr, { force = false })
-        end)
-      end,
-    })
 
     local lines
     if type(name_or_nr) == "number" then
       lines = buflines.iter(name_or_nr)
-    elseif type(name_or_nr) == "string" then
-      lines = io.lines(name_or_nr, "*l")
     else
-      error("unreaachable")
+      lines = io.lines(name_or_nr, "*l")
     end
 
     for line in lines do
       vim.fn.chansend(tid, line)
       vim.fn.chansend(tid, "\n")
     end
+
+    vim.fn.chanclose(tid)
   end
 
   local winid = bufopen(open_mode, bufnr)
